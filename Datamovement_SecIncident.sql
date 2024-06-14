b5895875fe5fb265392be0bb0a10f22e1092ef49
Lekden Namgyal <87456189+Lekden@users.noreply.github.com>
2024-06-14 13:19:50 +1000

    Update Datamovement_SecIncident.sql

--- Datamovement_SecIncident.sql
+++ Datamovement_SecIncident.sql
@@ -21,14 +21,8 @@ from mdl_groups mg
 join mdl_groups_members mgm on mgm.groupid = mg.id) zz
 on z.userid = zz.userid and z.courseid = zz.courseid
 where 
-z.wrong_username in ('andre.garing@defence.gov.au','keiya.kojima@defence.gov.au','trey.hubert@defence.gov.au','austin.nott@defence.gov.au','willem.taylor@defence.gov.au','lee.echivarre@defence.gov.au','lewis.stewart@defence.gov.au',
-'lily.masters@defence.gov.au',
-'samuel.belcastro@defence.gov.au','samantha.midson@defence.gov.au','chanel.albion@defence.gov.au','zara.hart@defence.gov.au','dylanger.honneysett@defence.gov.au','claudia.doeland@defence.gov.au',
-'isabelle.boeske@defence.gov.au','samuel.mclennan@defence.gov.au','makayla.magro@defence.gov.au','mitchell.hewitt1@defence.gov.au','joshua.eldred@defence.gov.au','jamie.shapiro@defence.gov.au','sean.hemmings@defence.gov.au','coby.saler@defence.gov.au',
-'michael.mayne@defence.gov.au','brent.hoffman@defence.gov.au','alexander.gillies@defence.gov.au','rebel.brooker@defence.gov.au','brandon.ebdon@defence.gov.au','georgia.pearson1@defence.gov.au','cameron.eales@defence.gov.au','darcy.knight@defence.gov.au',
-'rhys.devlin@defence.gov.au','samantha.claridge@defence.gov.au','nicholas.spencer@defence.gov.au','rory.goodison@defence.gov.au','blake.white@defence.gov.au','samuel.stephan@defence.gov.au','jordan.mccahon@defence.gov.au',
-'beau.redden@defence.gov.au','gurpreet.singh6@defence.gov.au','belinda.clape@defence.gov.au','oliver.agbisit@defence.gov.au','samuel.gorton@defence.gov.au','demas.farrell@defence.gov.au','harley.windham@defence.gov.au')
-and z.courseid in ('2342','45786','45242','37239','24636','40353','30646','45025','41695','2361','2363','2362','6851','43114','12717')
+z.wrong_username in ('')
+and z.courseid in ('2')
 and zz.group_added > '2023-06-13 09:20:00'
 and z.activity_completed > '2023-06-13 09:20:00'
 
