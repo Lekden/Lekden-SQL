4c8ec8574cc6ffe4d7ecdba8fa721bc3ec2ab795
Lekden Namgyal <87456189+Lekden@users.noreply.github.com>
2024-06-17 09:16:19 +1000

    Delete Script-7.sql

--- Script-7.sql
+++
@@ -1,5 +0,0 @@
-select * from mdl_logstore_standard_log mlsl, to_timestamp(timecreated) where objectid = 444
-and  to_timestamp(timecreated) >= '2023-04-27'
-
-
-select * from mdl_customcert_templates 
