114f7b5442d7b0f7337cf59144025ce367f1ced3
Lekden Namgyal <87456189+Lekden@users.noreply.github.com>
2024-06-19 09:32:50 +1000

    Delete Script-2.sql

--- Script-2.sql
+++
@@ -1,5 +0,0 @@
-select mt.id, mt.name, mc.* from mdl_tag_instance mti 
-join mdl_tag mt on mti.tagid = mt.id 
-join mdl_course mc on mti.itemid = mc.id 
-
--- permission teachers and managers by default. moodle/course:tag
